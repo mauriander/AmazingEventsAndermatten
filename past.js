34dda78d30248af272f7c83d39ca9cb904bacd04
mauriander <andermattenmauricio@gmail.com>
2023-08-28 19:23:07 -0300

    CREATE home.js, past.js upcoming.js MODIFIED styles.css

---
+++ past.js
@@ -0,0 +1,17 @@
+    for(let evento of data.events) {
+        if (evento.date < data.currentDate) {
+          let card = `<div class="card card-index">
+            <img src=${evento.image} class="card-img-top" alt=""/>
+              <div class="card-body">
+                <h5 class="card-title text-center">${evento.name}</h5>
+                <h6 class="card-date text-center">${evento.date}</h6>
+                <p class="card-text text-center">${evento.description}</p>
+              </div>
+              <div class="footer-card d-flex">
+                <P>Precio $${evento.price}</P>
+                <a href="./details.html" class="btn btn-see-more">Ver mas...</a>
+              </div>
+            </div>`
+          cardSection.innerHTML += card;
+        }
+    }
